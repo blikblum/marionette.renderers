55b5878427dac922ec0f0fc45638f44e88ea5bcc
Luiz Américo <brandkarl@gmail.com>
2017-05-27 11:26:11 -0300

    Make rivets example work

--- examples/rivets/src/contact/modeljsonview.js
+++ examples/rivets/src/contact/modeljsonview.js
@@ -1,11 +1,35 @@
+import $ from 'jquery'
+import Backbone from 'backbone'
 import Marionette from 'backbone.marionette'
 import template from './modeljson-tpl.html'
+import sampleData from './sampledata'
 
 export default Marionette.View.extend({
   attributes: {
     id: 'model-json-view'
   },
-  
-  
-  template: template
+
+  template: template,
+
+  events: {
+    'click a': 'changeSampleClick'
+  },
+
+  initialize(options){
+    if (!this.model) this.model = new Backbone.Model()
+    this.watched = options.watch;
+    this._setWatchedModelJSON();
+    return this.listenTo(this.watched, "change", this._setWatchedModelJSON);
+  },
+
+  _setWatchedModelJSON() {
+    let json = JSON.stringify(this.watched.toJSON(), null, '  ');
+    return this.model.set("modelJSON", json);
+  },
+
+  changeSampleClick(e) {
+    let sampleName = $(e.currentTarget).data().sample
+    this.watched.clear({silent: true})
+    this.watched.set(sampleData[sampleName])
+  }
 })
--- examples/rivets/src/main.js
+++ examples/rivets/src/main.js
@@ -10,20 +10,19 @@ let MainView = Marionette.View.extend({
   el: '#main-view',
   
   regions: {
-    contact: '#contact-view',
-    contactform: '#contact-form-view',
-    modeljson: '#model-json-view'
+    contact: {el: '#contact-view', replaceElement: true},
+    contactform: {el: '#contact-form-view', replaceElement: true},
+    modeljson: {el: '#model-json-view', replaceElement: true}
   },
   
   initialize() {
     this.showChildView('contact', new ContactView({model: this.model}))
     this.showChildView('contactform', new ContactFormView({model: this.model}))
-    this.showChildView('modeljson', new ModelJSONView({model: this.model}))
+    this.showChildView('modeljson', new ModelJSONView({watch: this.model}))
   }
 })
-
 
 let contact = new ContactModel()
 contact.set(sampleData.michael)
 
-new MainView()
+new MainView({model: contact})
