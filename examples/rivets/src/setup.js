c4fbb59d6bae4c2fa14808f866cdb5b7b5c8840f
Luiz Américo <brandkarl@gmail.com>
2017-05-27 11:25:24 -0300

    Import renderer from npm. Setup rivets

--- examples/rivets/src/setup.js
+++ examples/rivets/src/setup.js
@@ -1,6 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './main.scss'
-import renderer from '../../../src/rivets'
+import rivets from 'rivets'
+import 'rivets-backbone-adapter'
+import renderer from 'marionette.renderers/rivets'
 import Marionette from 'backbone.marionette'
 
 Marionette.View.setRenderer(renderer)
+
+// Custom Rivets formatter to replace text line breaks with <br>s
+// See formatter docs here: http://www.rivetsjs.com/docs/#formatters\
+
+rivets.formatters.linebreaksbr = function(value){
+  return value.replace(/\n/g, '<br>')
+};
