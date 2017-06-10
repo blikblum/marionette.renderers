81032a4b7d03517684fd017d27e9ab20137dfcd1
Luiz Américo <brandkarl@gmail.com>
2017-06-09 21:38:36 -0300

    snabbdom: allow to render root nodes

--- snabbdom.js
+++ snabbdom.js
@@ -1,18 +1,24 @@
 var snabbdom = require('snabbdom')
+var vnode = require('snabbdom/vnode').default
 var patch = snabbdom.init([ // Init patch function with chosen modules
   require('snabbdom/modules/attributes').default,
   require('snabbdom/modules/eventlisteners').default,
   require('snabbdom/modules/class').default,
   require('snabbdom/modules/props').default,
-  require('snabbdom/modules/style').default
+  require('snabbdom/modules/style').default,
+  require('snabbdom/modules/dataset').default
 ])
 
 module.exports = function (template, data) {
-  var newVirtualTree = template(data)
-  if (!this.elVirtualTree) {
-    patch(this.el, newVirtualTree)
-    this.elVirtualTree = newVirtualTree
+  var newTree = template(data)
+  if (!this.elTree) {
+    // small cheat to allow rendering root el
+    // creates an empty vnode with the same sel as the rendered vtree
+    // this ensure the view element will be properly patched
+    var emptyTree = vnode(newTree.sel, {}, [], undefined, this.el)
+    patch(emptyTree, newTree)
+    this.elTree = newTree
   } else {
-    patch(this.elVirtualTree, newVirtualTree)
+    patch(this.elTree, newTree)
   }
 }
