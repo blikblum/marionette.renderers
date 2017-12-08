f3141cbd245683b191a16640ac05479b90931b8e
Luiz Américo <luizamericop@gmail.com>
2017-12-07 21:23:27 -0300

    Implement unmounting react components

--- react.js
+++ react.js
@@ -1,6 +1,14 @@
-var render = require('react-dom').render
+var ReactDOM = require('react-dom')
+
+function unmountReact() {
+  ReactDOM.unmountComponentAtNode(this.el)
+}
 
 module.exports = function (template, data) {
+  if (!this._reactMounted) {
+    this.on('destroy', unmountReact, this)
+    this._reactMounted = true
+  }
   var state = this.thisAsState ? this : data
-  render(template.call(this, state), this.el)
+  ReactDOM.render(template.call(this, state), this.el)
 }
