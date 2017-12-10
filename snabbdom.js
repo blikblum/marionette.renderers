d368554f4e483efb3fa1cf4fa5a44dda63fe4d0c
Luiz Américo <luizamericop@gmail.com>
2017-12-10 09:05:49 -0300

    Snabbdom: return a factory instead of a renderer (allows customize the modules)

--- snabbdom.js
+++ snabbdom.js
@@ -1,31 +1,26 @@
-var snabbdom = require('snabbdom')
+var init = require('snabbdom').init
 var vnode = require('snabbdom/vnode').default
-var patch = snabbdom.init([ // Init patch function with chosen modules
-  require('snabbdom/modules/attributes').default,
-  require('snabbdom/modules/eventlisteners').default,
-  require('snabbdom/modules/class').default,
-  require('snabbdom/modules/props').default,
-  require('snabbdom/modules/style').default,
-  require('snabbdom/modules/dataset').default
-])
 
-module.exports = function (template, data) {
-  var state = this.thisAsState ? this : data
-  var newTree = template.call(this, state), rootTag
-  if (this.outerRender) {
-    rootTag = newTree.sel
-  } else {
-    rootTag = this.tagName
-    newTree = vnode(rootTag, {}, Array.isArray(newTree) ? newTree : [newTree], undefined, this.el)
+module.exports = function createRenderer (modules) {
+  var patch = init(modules)
+  return function renderTree (template, data) {
+    var state = this.thisAsState ? this : data
+    var newTree = template.call(this, state), rootTag
+    if (this.outerRender) {
+      rootTag = newTree.sel
+    } else {
+      rootTag = this.tagName
+      newTree = vnode(rootTag, {}, Array.isArray(newTree) ? newTree : [newTree], undefined, this.el)
+    }
+    if (!this.elTree) {
+      // small cheat to allow rendering root el
+      // creates an empty vnode with the same sel as the rendered vtree
+      // this ensure the view element will be properly patched
+      var emptyTree = vnode(rootTag, {}, [], undefined, this.el)
+      patch(emptyTree, newTree)
+    } else {
+      patch(this.elTree, newTree)
+    }
+    this.elTree = newTree
   }
-  if (!this.elTree) {
-    // small cheat to allow rendering root el
-    // creates an empty vnode with the same sel as the rendered vtree
-    // this ensure the view element will be properly patched
-    var emptyTree = vnode(rootTag, {}, [], undefined, this.el)
-    patch(emptyTree, newTree)
-  } else {
-    patch(this.elTree, newTree)
-  }
-  this.elTree = newTree
 }
